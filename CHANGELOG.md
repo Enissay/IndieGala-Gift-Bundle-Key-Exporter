bf01b11e38def3278c26e738ee0491e2171cf70f
enissay <yassinefpms@gmail.com>
2022-02-20 16:09:13 +0100

    Safe injection of jQuery and use with $ Extract steam game link and add it to the copied string

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [1.3.0] - 2022-02-20
+
+### Changed
+
+Safe injection of jQuery and use with $ Extract steam game link and add it to the copied string
+
+## [1.2.0] - 2022-02-20
+
+### Changed
+
+Forked original version to customize it & migrate to jQeury+Selectors
+https://github.com/mdnpascual/IndieGala-Gift-Bundle-Key-Exporter
+
+## [1.2.3] - 2017-03-14 - Template
+
+### Added
+
+### Changed
+
+### Fixed
+
+- [PROJECTNAME-UUUU](http://tickets.projectname.com/browse/PROJECTNAME-UUUU)
+  MINOR Fix module foo tests
+- [PROJECTNAME-RRRR](http://tickets.projectname.com/browse/PROJECTNAME-RRRR)
+  MAJOR Module foo's timeline uses the browser timezone for date resolution 
--- IndieGala_Gift_Bundle_Key_Exporter.user.js
+++ IndieGala_Gift_Bundle_Key_Exporter.user.js
@@ -1,68 +1,76 @@
 // ==UserScript==
-// @name         Indie Gala Gift Bundle Key Exporter
+// @name         Indie Gala Gift Bundle Key Exporter - Enhanced
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3.0
 // @description  1-Click Key Export for your IndieGala Gifts (Updated for Indiegala's website redesign on May 2020)
-// @author       MDuh
+// @author       MDuh - Eny
 // @match        https://www.indiegala.com/gift-bundle/*
 // @match        https://www.indiegala.com/gift?gift_id=*
-// @supportURL   https://github.com/mdnpascual/IndieGala-Gift-Bundle-Key-Exporter/issues
-// @updateURL    https://github.com/mdnpascual/IndieGala-Gift-Bundle-Key-Exporter/raw/master/IndieGala_Gift_Bundle_Key_Exporter.user.js
-// @downloadURL  https://github.com/mdnpascual/IndieGala-Gift-Bundle-Key-Exporter/raw/master/IndieGala_Gift_Bundle_Key_Exporter.user.js
+// @supportURL   https://github.com/Enissay/IndieGala-Gift-Bundle-Key-Exporter/issues
+// @updateURL    https://github.com/Enissay/IndieGala-Gift-Bundle-Key-Exporter/raw/master/IndieGala_Gift_Bundle_Key_Exporter.user.js
+// @downloadURL  https://github.com/Enissay/IndieGala-Gift-Bundle-Key-Exporter/raw/master/IndieGala_Gift_Bundle_Key_Exporter.user.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
-var zNode       = document.createElement ('li');
-zNode.className = "active bg-gradient-light-blue"
-zNode.innerHTML = '<div class="profile-private-page-library-menu-item-inner"><a id="copyKeyButton" href="#" onclick="return false">Redeem Keys</a></div>';
-zNode.setAttribute ('id', 'myContainer');
+// safe jQuery loading header
+(function ($, undefined) {
+    $(function () {
+////////////////////////////////////
 
-var injectLocation = document.getElementsByClassName("profile-private-page-library-menu")[0].children[0];
-injectLocation.appendChild (zNode);
-injectLocation.style = "justify-content: space-between; display: flex;"
+        var zNode = document.createElement('li');
+        zNode.className = "active bg-gradient-light-blue"
+        zNode.innerHTML = '<div class="profile-private-page-library-menu-item-inner"><a id="copyKeyButton" href="#" onclick="return false">Redeem Keys</a></div>';
+        zNode.setAttribute('id', 'myContainer');
+
+        var injectLocation = document.getElementsByClassName("profile-private-page-library-menu")[0].children[0];
+        injectLocation.appendChild(zNode);
+        injectLocation.style = "justify-content: space-between; display: flex;"
 
 //--- Activate the newly added button.
-document.getElementById ("copyKeyButton").addEventListener (
-    "click", main, false
-);
+        document.getElementById("copyKeyButton").addEventListener(
+            "click", main, false
+        );
 
-function main(){
+        function main() {
 
-    // ACTIVATE ALL KEYS
-    var buttonToRedeem = document.querySelectorAll("figcaption > div.profile-private-page-library-serial-dialog.display-none > div > button.right.profile-private-page-library-get-serial-btn.bg-gradient-blue");
+            // ACTIVATE ALL KEYS
+            var buttonToRedeem = document.querySelectorAll("figcaption > div.profile-private-page-library-serial-dialog.display-none > div > button.right.profile-private-page-library-get-serial-btn.bg-gradient-blue");
 
-    for(let item of buttonToRedeem){
-        item.click();
-        window.getSerialKeyGo = true;
-    }
+            for (let item of buttonToRedeem) {
+                item.click();
+                window.getSerialKeyGo = true;
+            }
 
-    // KEEP CHECKING UNTIL NODE IS ZERO
-    var isItDoneYet = setInterval(function() {
-        var checkDOM = document.querySelectorAll("figcaption > div.profile-private-page-library-serial-dialog.display-none > div > button.right.profile-private-page-library-get-serial-btn.bg-gradient-blue");
+            // KEEP CHECKING UNTIL NODE IS ZERO
+            var isItDoneYet = setInterval(function () {
+                var checkDOM = document.querySelectorAll("figcaption > div.profile-private-page-library-serial-dialog.display-none > div > button.right.profile-private-page-library-get-serial-btn.bg-gradient-blue");
 
-        if (checkDOM.length === 0) {
-            clearInterval(isItDoneYet);
-            generateResult();
-        }
-    }, 200);
+                if (checkDOM.length === 0) {
+                    clearInterval(isItDoneYet);
+                    generateResult();
+                }
+            }, 200);
 
 }
 
-function generateResult(){
-    var keyResult = "";
+        function generateResult() {
+            var keyResult = "";
 
-    // GET GAME LIST
-    var gameList = document.evaluate("//li/div/figcaption/div[div[contains(@class, 'left')]]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-    for ( var i=0 ; i < gameList.snapshotLength; i++ )
-    {
-        // APPEND GAME:KEY LIST
-        keyResult +=
-            gameList.snapshotItem(i).children[0].children[0].innerHTML  // GAME NAME
-            + ":\t" +
-            gameList.snapshotItem(i).children[1].children[0].children[0].children[0].value // SERIAL KEY
-            + "\r\n";
-        // MODIFY HTML TO SET STEAM ICON AS A URL TO ACTIVATE THE KEY TO STEAM
-        gameList.snapshotItem(i).children[1].children[0].children[1].setAttribute("onClick", "javascript: window.open('https://store.steampowered.com/account/registerkey?key=" + gameList.snapshotItem(i).children[1].children[0].children[0].children[0].value + "')");
+            // GET GAME LIST
+            var gameList = document.evaluate("//li/div/figcaption/div[div[contains(@class, 'left')]]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+            var gameList_eny = $("ul.profile-private-page-library-sublist.profile-private-page-library-sublist-active > li");
+            for (var i = 0; i < gameList.snapshotLength; i++) {
+                // APPEND GAME:KEY LIST
+                keyResult +=
+                    gameList.snapshotItem(i).children[0].children[0].innerHTML  // GAME NAME
+                    + "\t" +
+                    gameList.snapshotItem(i).children[1].children[0].children[0].children[0].value // SERIAL KEY
+                    + ":\t" +
+                    gameList_eny.eq(i).find('a[href*="https://store.steampowered.com/"]').attr('href')  // GAME URL
+                    + "\r\n";
+                // MODIFY HTML TO SET STEAM ICON AS A URL TO ACTIVATE THE KEY TO STEAM
+                gameList.snapshotItem(i).children[1].children[0].children[1].setAttribute("onClick", "javascript: window.open('https://store.steampowered.com/account/registerkey?key=" + gameList.snapshotItem(i).children[1].children[0].children[0].children[0].value + "')");
         gameList.snapshotItem(i).children[1].children[0].children[1].setAttribute("class", "right profile-private-page-library-key-icon bg-gradient-blue");
         gameList.snapshotItem(i).children[1].children[0].children[1].style = "cursor: pointer;"
     }
@@ -70,13 +78,18 @@ function generateResult(){
     textToClipboard(keyResult);
 
     alert("Game keys copied to clipboard");
-}
+        }
 
-function textToClipboard (text) {
-    var dummy = document.createElement("textarea");
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
-}
+        function textToClipboard(text) {
+            var dummy = document.createElement("textarea");
+            document.body.appendChild(dummy);
+            dummy.value = text;
+            dummy.select();
+            document.execCommand("copy");
+            document.body.removeChild(dummy);
+        }
+
+////////////////////////////////////
+// safe jQuery loading footer
+    });
+})(window.jQuery.noConflict(true));
